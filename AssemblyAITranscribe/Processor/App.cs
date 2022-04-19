9d810a7ae65a82772d2baa36a6cb26c052223a63
rahiyansafz <rahiyansafin@gmail.com>
2022-04-20 04:51:38 +0600

    In Progress

--- AssemblyAITranscribe/Processor/App.cs
+++ AssemblyAITranscribe/Processor/App.cs
@@ -8,8 +8,40 @@ namespace AssemblyAITranscribe.Processor;
 
 internal class App
 {
+    static string API_Key = "15b3b6ce5290439286100190e09f35b2";
     public static void Run()
     {
         Console.WriteLine("Hello, World!");
+
+        HttpClient client = new();
+
+        client.BaseAddress = new Uri("https://api.assemblyai.com/v2/");
+        client.DefaultRequestHeaders.Add("authorization", API_Key);
+
+        string jsonResult = SendFile(client, @"E:\Downloads HDD\lana-diet-mountain.mp3").Result;
+        Console.WriteLine(jsonResult);
+    }
+
+    public static async Task<string> SendFile(HttpClient client, string filePath)
+    {
+        try
+        {
+            HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, "upload");
+            httpRequest.Headers.Add("Transfer-Encoding", "chunked");
+
+            var fileReader = File.OpenRead(filePath);
+            var streamContent = new StreamContent(fileReader);
+
+            httpRequest.Content = streamContent;
+
+            HttpResponseMessage responseMessage = await client.SendAsync(httpRequest);
+
+            return await responseMessage.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+            throw;
+        }
     }
 }
