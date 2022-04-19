2843141518a87ac8294121a0ca5c6d70285b0a21
rahiyansafz <rahiyansafin@gmail.com>
2022-04-20 04:13:58 +0600

    Folder Structured

---
+++ AssemblyAITranscribe/Processor/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyAITranscribe.Processor;
+
+internal class App
+{
+    public static void Run()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
